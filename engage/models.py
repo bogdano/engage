32b7f05dd142656371d8645549a16acc7fe58542
Jerico <jericowaller@gmail.com>
2024-02-10 11:53:27 -0700

    Shop database

--- engage/models.py
+++ engage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # model for activities
 class Activity(models.Model):
     # title string
@@ -7,7 +8,7 @@ class Activity(models.Model):
     # description string
     description = models.TextField()
     # creator_id foreign key to custom user model
-    creator_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    creator_id = models.ForeignKey("User", on_delete=models.CASCADE)
     # location string
     location = models.CharField(max_length=200)
     # event_date date
@@ -15,7 +16,7 @@ class Activity(models.Model):
     # created_at date
     created_at = models.DateField(auto_now_add=True)
     # activity_type fk to activity type model
-    activity_type = models.ForeignKey('ActivityType', on_delete=models.CASCADE)
+    activity_type = models.ForeignKey("ActivityType", on_delete=models.CASCADE)
     # photo field string
     photo = models.CharField(max_length=200)
 
@@ -23,22 +24,29 @@ class Activity(models.Model):
 class ActivityType(models.Model):
     pass
 
+
 class Item(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField(max_length=10)
 
 
-class itemColor(models.Model):
-    pass
-  
+class ItemColors(models.Model):
+    color = models.CharField(max_length=200)
+
+
+class ItemSizes(models.Model):
+    size = models.CharField(max_length=200)
+
+
 class UserParticipates(models.Model):
     # user_id fk to custom user model
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
     # activity_id fk to activity model
-    activity_id = models.ForeignKey('Activity', on_delete=models.CASCADE)
+    activity_id = models.ForeignKey("Activity", on_delete=models.CASCADE)
+
 
 class UserInterested(models.Model):
     # user_id fk to custom user model
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
     # activity_id fk to activity model
-    activity_id = models.ForeignKey('Activity', on_delete=models.CASCADE)
+    activity_id = models.ForeignKey("Activity", on_delete=models.CASCADE)
