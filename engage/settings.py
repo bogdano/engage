f734b40be2183737aaa5683842c554520d063ca3
bogdano <bogdan.boskovic@outlook.com>
2024-04-08 20:27:49 -0600

    minor fixes, preserve image on activity edit, prevent negative point values

--- engage/settings.py
+++ engage/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SET DEFAULT TO FALSE, FOR DEPLOYMENT !!!!!!!!!!!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = [".fly.dev", "localhost", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
